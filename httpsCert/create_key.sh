dc0472de55b6cc11ca6aecd29c63a358c801e763
Hans Hörberg <hans.horberg@umu.se>
2014-01-10 14:44:28 +0100

    Added key generation script.

--- httpsCert/create_key.sh
+++ httpsCert/create_key.sh
@@ -1,5 +1,5 @@
-openssl genrsa -des3 -out sp.key 1024
-openssl req -new -key sp.key -out sp.csr
-cp sp.key sp.key.org
-openssl rsa -in sp.key.org -out sp.key
-openssl x509 -req -days 365 -in sp.csr -signkey sp.key -out sp.crt
+openssl genrsa -des3 -out server.key 1024
+openssl req -new -key server.key -out server.csr
+cp server.key server.key.org
+openssl rsa -in server.key.org -out server.key
+openssl x509 -req -days 365 -in server.csr -signkey server.key -out server.crt
